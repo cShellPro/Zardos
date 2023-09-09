c40e1077b9fceb87d2dabecc37a857e9676d9c31
cShellPro <79190115+cShellPro@users.noreply.github.com>
2023-09-09 15:09:21 +0200

    Zardos.sol Smart Contract

--- Zardos.sol
+++ Zardos.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // contract to create ERC20 token Zardos (ZARS)
 // token has 18 decimals
 // initial supply 1 Mio. token
-// maximum supply 21 Mio. token
+// maximum supply 25 Mio. token
 contract Zardos is ERC20{
     constructor(uint256 initialSupply) ERC20("Zardos", "ZARS") {
        _mint(msg.sender,initialSupply);
